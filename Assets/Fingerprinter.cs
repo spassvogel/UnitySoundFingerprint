633f120e3643485fc0feca807d64367632b66e14
Wouter van den Heuvel <exo_00@hotmail.com>
2018-05-27 21:20:41 +0200

    Fixed up

--- Assets/Fingerprinter.cs
+++ Assets/Fingerprinter.cs
@@ -14,23 +14,21 @@ using SoundFingerprinting.Query;
 
 public class Fingerprinter : MonoBehaviour {
 	
-    public float trimCutoff = .01f;  // Minimum volume of the clip before it gets trimmed    
+    public float trimCutoff = .01f;  	// Minimum volume of the clip before it gets trimmed    
+
 
 	private AudioSource audioSource; 
 	private string micName = null;
 	private int sampleTime = 3;
 	private int sampleRate = 44100;
 
-
-
 	private float[] mAudioBuffer = null;
-
 
 	private readonly IModelService modelService = new InMemoryModelService(); // store fingerprints in RAM
 	private readonly IAudioService audioService = new SoundFingerprintingAudioService(); // default audio library
 	
 	void Start() {
-		TestFileSystem();
+		StoreAudioFileFingerprintsInStorageForLaterRetrieval(Path.Combine(Application.streamingAssetsPath, "sweden.wav"));
 
 		if (Microphone.devices == null || Microphone.devices.Length < 1) {
 			Debug.LogError ("Microphone init error");
@@ -39,40 +37,12 @@ public class Fingerprinter : MonoBehaviour {
 		micName = Microphone.devices[0];
 
 		audioSource = this.gameObject.AddComponent<AudioSource>();
-//		audioSource.Play();
 
 		StartRecord();
 	}
-
-	private void TestFileSystem() {
-		StoreAudioFileFingerprintsInStorageForLaterRetrieval(Path.Combine(Application.streamingAssetsPath, "sweden.wav"));
-
-		var match = GetBestMatchForFile(Path.Combine(Application.streamingAssetsPath, "sweden_rec.wav"));
-		if(match != null){
-			Debug.Log("Found track " + match.Track.Title + " with confidence: " + match.Confidence);
-		}
-	}
-
-	
-	public ResultEntry GetBestMatchForFile(string queryAudioFile, int secondsToAnalyze = 5, int startAtSecond = 0)
-	{
-	
-		// query the underlying database for similar audio sub-fingerprints
-		var queryResult = QueryCommandBuilder.Instance.BuildQueryCommand()
-			.From(queryAudioFile, secondsToAnalyze, startAtSecond)
-			.UsingServices(modelService, audioService)
-			.Query()
-			.Result;
-		
-		return queryResult.BestMatch; // successful match has been found
-	}
-
-	
-
 	
 	public ResultEntry GetBestMatchForAudioClip(AudioClip clip)
 	{	
-		// This doesn't work :/	
 		var audioSamples = AudioClipBridge.AudioClipToAudioSamples(clip);
 
 		// query the underlying database for similar audio sub-fingerprints
@@ -127,18 +97,7 @@ public class Fingerprinter : MonoBehaviour {
 			print("Clip trimmed to 0");
 			return;
 		}       
-		 AudioClipBridge.AudioClipToAudioSamples(trimmedClip);  
-
-		var samples = new float[trimmedClip.samples];
-        trimmedClip.GetData(samples, 0);
-
-		// Save recording
-		var filename = "Recording";
-		SavWav.Save(filename, trimmedClip);
-
-		// TODO: Make it work with an AudioClip!
-		//var match = GetBestMatchForFile(Path.Combine(Application.persistentDataPath, filename + ".wav"));
-		var match = GetBestMatchForAudioClip(trimmedClip);
+		ResultEntry match = GetBestMatchForAudioClip(trimmedClip);
 		
 		if(match != null){
 			Debug.Log("Found track " + match.Track.Title + " with confidence: " + match.Confidence);
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 
-Trying to figure out how to link `soundfingerprinting` (https://github.com/AddictedCS/soundfingerprinting) c# library to Unity.
+This is a test suite linking `soundfingerprinting` (https://github.com/AddictedCS/soundfingerprinting) c# library to Unity.
 
 
 
@@ -11,7 +11,5 @@ How to run?
 * Ensure a microphone and speakers are present and working
 * Play `Assets/StreamingAssets/sweden_5s.wav` in a media player on repeat
 * Run the Unity project in the Unity Editor
-* Every 3 seconds it will attempt to match the sound it hears through the microphone with the stored sample
+* Every 3 seconds it will attempt to match the sound it hears through the microphone with the stored sample (see Console)
 
-
-Note that it works if you call `GetBestMatchForFile` instead of `GetBestMatchForAudioClip` but obviously I would like to see it working without file IO. I would like `AudioClipBridge.AudioClipToAudioSamples` to return the `AudioSamples` correctly..
